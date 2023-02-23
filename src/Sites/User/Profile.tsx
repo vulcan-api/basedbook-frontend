9c0976e082a55f4b4cfa52570b3dace815c358db
Tomasz Mamala <tomaszmamala@gmail.com>
2023-02-23 22:59:40 +0100

    fixes of profile

--- src/Sites/User/Profile.tsx
+++ src/Sites/User/Profile.tsx
@@ -23,7 +23,7 @@ const Profile = () => {
             <div className={classes.managementContainer}>
                 <div className={classes.detailsContainer}>
                     <h2>Twoja Mama 69</h2>
-                    <h2 className={classes.ovColor}>Oliwka Brazil klasa 2p</h2>
+                    <p className={classes.ovColor}>Oliwka Brazil klasa 2p</p>
                     <p>Bardzo śmieszny opis tej jakże śmiesznej baby.</p>
                 </div>
                 <div className={classes.buttonsArea}>
@@ -31,20 +31,6 @@ const Profile = () => {
                     <Button buttonText={<span style={{display: 'flex', flexDirection: 'row', columnGap: ".3rem"}}>Sociale <Instagram style={{width: '1.2rem', height: '1.2rem', paddingTop: '.3rem'}}/></span>} className="alternate"/>
                 </div>
             </div>
-          <div className={classes.avatar}>
-            <img className={classes.avImage} src={turtle} alt="" />
-          </div>
-          <div className={classes.managementContainer}>
-            <div className={classes.detailsContainer}>
-              <h2>Twoja Mama 69</h2>
-              <p className={classes.ovColor}>Oliwka Brazil klasa 2p</p>
-              <p>Bardzo śmieszny opis tej jakże śmiesznej baby.</p>
-            </div>
-            <div className={classes.buttonsArea}>
-              <Button buttonText="Dodaj do znajomych" />
-              <Button buttonText="Sociale" className="alternate" />
-            </div>
-          </div>
         </div>
         <div className={classes.creations}>
           <Button
