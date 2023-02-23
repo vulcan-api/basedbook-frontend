ab170dc2b143fa9c0b3b98d339169c2594e3a8d2
cooligus <pewienjaaa@gmail.com>
2023-02-23 22:42:57 +0100

    🦍 Add very powerful notification

--- src/Sites/User/Profile.tsx
+++ src/Sites/User/Profile.tsx
@@ -3,6 +3,8 @@ import classes from "./Profile.module.css";
 import turtle from "./Graphics/turtle.jpg"
 import Button from "../../Components/Button";
 import { Instagram } from "react-bootstrap-icons";
+//@ts-ignore
+import {NotificationManager} from "react-notifications";
 
 const Profile = () => {
     const [isSended, setIsSended] = useState(false);
@@ -18,7 +20,8 @@ const Profile = () => {
                     <p>Bardzo śmieszny opis tej jakże śmiesznej baby.</p>
                 </div>
                 <div className={classes.buttonsArea}>
-                    <Button buttonText="Dodaj do znajomych" disabled={isSended} onClick={()=>{setIsSended(true)}}/>
+                    <Button buttonText="Dodaj do znajomych" disabled={isSended} onClick={()=>{setIsSended(true); NotificationManager.success("Udało się zaprosić użytkownika do znajomych.", "Sukces!", 3000);
+}}/>
                     <Button buttonText={<span style={{display: 'flex', flexDirection: 'row', columnGap: ".3rem"}}>Sociale <Instagram style={{width: '1.2rem', height: '1.2rem', paddingTop: '.3rem'}}/></span>} className="alternate"/>
                 </div>
             </div>
