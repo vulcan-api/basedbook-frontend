13ce3d4bf329bf033d2e5c51ddd43e7b219218ab
Maksymilian Gala <mgwspinanie2006@wp.pl>
2023-02-25 19:59:59 +0100

    Fix bug with applying to project

--- src/Sites/Project/Project.tsx
+++ src/Sites/Project/Project.tsx
@@ -1,42 +1,44 @@
-import React, { useEffect } from "react";
+import React, {useEffect} from "react";
 import classes from "./Project.module.css";
 import Button from "../../Components/Button";
 import * as Icon from "react-bootstrap-icons";
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import {useState} from "react";
+import {Link} from "react-router-dom";
 import LoadingSpinner from "../../Components/LoadingSpinner";
 import Modal from "../../Layout/Modal";
 import ProjectItem from "./ProjectItem";
+//@ts-ignore
+import {NotificationManager} from "react-notifications";
 
 const Project = () => {
-  const [projects, setProject] = useState([
-    {
-      id: 1,
-      createdAt: new Date("2023-02-14T18:09:09.433Z"),
-      title: "BusinessAssistant+",
-      text: "Hej! Szukamy ludzi do przepisania naszego projektu w JS/TS",
-      username: "Seweryn Pajor",
-    },
-  ]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [showModal, setShowModal] = useState(false);
-  const [reportedProjectId, setReportedProjectId] = useState(-100);
-  const [listType, setListType] = useState({
-    width: "40%",
-  });
-
-  const [isActive, setIsActive] = useState(true);
-
-  function changeListTypeHandler(length: Number, id: Number) {
-    setIsActive(!id);
-    setListType({
-      width: length + "%",
+    const [projects, setProject] = useState([
+        {
+            id: 1,
+            createdAt: new Date("2023-02-14T18:09:09.433Z"),
+            title: "BusinessAssistant+",
+            text: "Hej! Szukamy ludzi do przepisania naszego projektu w JS/TS",
+            username: "Seweryn Pajor",
+        },
+    ]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [showModal, setShowModal] = useState(false);
+    const [reportedProjectId, setReportedProjectId] = useState(-100);
+    const [listType, setListType] = useState({
+        width: "40%",
     });
-  }
 
-  useEffect(() => {
-    getAllProjects();
-  }, []);
+    const [isActive, setIsActive] = useState(true);
+
+    function changeListTypeHandler(length: Number, id: Number) {
+        setIsActive(!id);
+        setListType({
+            width: length + "%",
+        });
+    }
+
+    useEffect(() => {
+        getAllProjects();
+    }, []);
 
     async function getAllProjects() {
         setIsLoading(true);
@@ -53,27 +55,27 @@ const Project = () => {
         setIsLoading(false);
     }
 
-    // async function applyToProject(event: any, id: any) {
-    //     event.preventDefault();
-    //     console.log(id);
-    //     const applyProject = {
-    //         projectId: id,
-    //     }
-    //     const throwObject = {};
-    //     const project = await fetch("http://localhost:3000/project/apply", {
-    //         method: "POST",
-    //         headers: {
-    //             "Content-Type": "application/json",
-    //         },
-    //         credentials: "include",
-    //         body: JSON.stringify(applyProject),
-    //     })
-    //         .then((res) => res.json()).then(console.log)
-    //         .catch((err) => {
-    //             console.error(err);
-    //             return throwObject;
-    //         });
-    // }
+    async function applyToProject(event: any, id: any) {
+        event.preventDefault();
+        console.log(id);
+        const applyProject = {
+            projectId: id,
+        }
+        const throwObject = {};
+        const response = await fetch("http://localhost:3000/project/apply", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            credentials: "include",
+            body: JSON.stringify(applyProject),
+        });
+        if (response.ok) {
+            NotificationManager.success("Udało się zgłosić do projektu.", "Sukces!", 3000);
+        } else {
+            NotificationManager.error("Zgłosiłeś się już do tego projektu!.", "Błąd!", 3000);
+        }
+    }
 
     const closeModal = () => {
         setShowModal(false);
@@ -106,13 +108,13 @@ const Project = () => {
                 <div className={classes.posts}>
                     {projects.map((project) => {
                         return (
-                          <div key={project.id} style={listType}>
-                            <ProjectItem
-                              project={project}
-                              openModal={openModal}
-                            //   applyToProject={applyToProject}
-                            />
-                          </div>
+                            <div key={project.id} style={listType}>
+                                <ProjectItem
+                                    project={project}
+                                    openModal={openModal}
+                                    applyToProject={applyToProject}
+                                />
+                            </div>
                         );
                     })}
                 </div>
