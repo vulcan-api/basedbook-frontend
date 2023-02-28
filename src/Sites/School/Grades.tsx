031c339b4e3b8f88416e812323f615ab196ef165
Tomasz Mamala <tomaszmamala@gmail.com>
2023-02-28 10:12:03 +0100

    still working on last grades

--- src/Sites/School/Grades.tsx
+++ src/Sites/School/Grades.tsx
@@ -1,83 +1,83 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import classes from "./Grades.module.css";
 import Checkbox from "../../Components/Checkbox";
 import LoadingSpinner from "../../Components/LoadingSpinner";
 // @ts-ignore
-import {NotificationManager} from "react-notifications";
-import {useNavigate} from "react-router-dom";
+import { NotificationManager } from "react-notifications";
+import { useNavigate } from "react-router-dom";
 
 const Grades = () => {
-    const [grades, setGrades] = useState<any[]>([]);
-    const [isActive, setIsActive] = useState(true);
-    const [isLoading, setIsLoading] = useState(false);
-    const navigate = useNavigate();
+  const [grades, setGrades] = useState<any[]>([]);
+  const [isActive, setIsActive] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
-    function changeListTypeHandler(id: Number) {
-        setIsActive(!id);
+  function changeListTypeHandler(id: Number) {
+    setIsActive(!id);
+  }
+
+  const getAllGrades = useCallback(async function () {
+    setIsLoading(true);
+    const gradesFetch = await fetch("http://localhost:3000/school/grades", {
+      method: "GET",
+      credentials: "include",
+    });
+
+    if (gradesFetch.status === 200) {
+      setGrades(await gradesFetch.json());
+    } else {
+      NotificationManager.error(
+        "Połącz swój dziennik w ustawieniach!",
+        "Błąd!",
+        3000
+      );
+      navigate("/");
     }
+    setIsLoading(false);
+  }, [navigate]);
 
-    useEffect(() => {
-        getAllGrades();
-    }, []);
+  useEffect(() => {
+    getAllGrades();
+  }, [getAllGrades]);
 
-    async function getAllGrades() {
-        setIsLoading(true);
-        const gradesFetch = await fetch("http://localhost:3000/school/grades", {
-            method: "GET",
-            credentials: "include",
-        });
-
-        if (gradesFetch.status === 200) {
-            setGrades(await gradesFetch.json());
-        } else {
-            NotificationManager.error(
-                "Połącz swój dziennik w ustawieniach!",
-                "Błąd!",
-                3000
+  return (
+    <>
+      <div className={classes.menu}>
+        <Checkbox
+          className={isActive ? "" : classes.active}
+          type="radio"
+          name="periodSwitch"
+          id="firstPeriod"
+          label="Okres 1"
+          onClick={() => changeListTypeHandler(1)}
+        />
+        <Checkbox
+          className={isActive ? classes.active : ""}
+          type="radio"
+          name="periodSwitch"
+          id="secondPeriod"
+          label="Okres 2"
+          onClick={() => changeListTypeHandler(0)}
+          defaultChecked
+        />
+      </div>
+      {!isLoading && (
+        <div>
+          {Object.keys(grades).map((subject: any) => {
+            return (
+              <div key={subject} className={classes.subject}>
+                {subject + ": "}
+                {grades[subject].map((grade1: any) => {
+                  return <span key={grade1.id}>{grade1.grade} </span>;
+                })}
+              </div>
             );
-            navigate("/");
-        }
-        setIsLoading(false);
-    }
-
-    return (
-        <>
-            <div className={classes.menu}>
-                <Checkbox
-                    className={isActive ? "" : classes.active}
-                    type="radio"
-                    name="periodSwitch"
-                    id="firstPeriod"
-                    label="Okres 1"
-                    onClick={() => changeListTypeHandler(1)}
-                />
-                <Checkbox
-                    className={isActive ? classes.active : ""}
-                    type="radio"
-                    name="periodSwitch"
-                    id="secondPeriod"
-                    label="Okres 2"
-                    onClick={() => changeListTypeHandler(0)}
-                    defaultChecked
-                />
-            </div>
-            {!isLoading && (
-                <div>
-                    {Object.keys(grades).map((subject: any) => {
-                        return (
-                            <div key={subject} className={classes.subject}>
-                                {subject + ": "}
-                                {grades[subject].map((grade1: any) => {
-                                    return <span key={grade1.id}>{grade1.grade} </span>;
-                                })}
-                            </div>
-                        );
-                    })}
-                </div>
-            )}
-            {isLoading && <LoadingSpinner/>}
-        </>
-    );
+          })}
+        </div>
+      )}
+      {isLoading && <LoadingSpinner />}
+    </>
+  );
 };
 
 export default Grades;
--- src/Sites/School/SchoolSidebar.tsx
+++ src/Sites/School/SchoolSidebar.tsx
@@ -1,148 +1,149 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, { useEffect, useRef, useState, useCallback } from "react";
 import LinkSection from "../../Components/LinkSection";
 import * as Icon from "react-bootstrap-icons";
 import classes from "./SchoolSidebar.module.css";
 
 const Sidebar = () => {
-    const navbarRef = useRef<HTMLDivElement>(null);
-    const [grades, setGrades] = useState<any[]>([]);
-    const [lastGrades, setLastGrades] = useState<any[]>([]);
-    const [luckyNumber, setLuckyNumber] = useState();
+  const navbarRef = useRef<HTMLDivElement>(null);
+  const [grades, setGrades] = useState<any[]>([]);
+  const [lastGrades, setLastGrades] = useState<any[]>([]);
+  const [temp, setTemp] = useState<any[]>([]);
+  const [luckyNumber, setLuckyNumber] = useState();
 
-    async function getAllGrades() {
-        try {
-            await fetch("http://localhost:3000/school/grades", {
-                method: "GET",
-                credentials: "include",
-            })
-                .then((res) => res.json())
-                .then((data) => setGrades(data.grades));
-        } catch (error) {
-            console.error(error);
-        }
+  async function getAllGrades() {
+    try {
+      await fetch("http://localhost:3000/school/grades", {
+        method: "GET",
+        credentials: "include",
+      })
+        .then((res) => res.json())
+        .then((data) => setGrades(data.grades));
+    } catch (error) {
+      console.error(error);
     }
+  }
 
-    async function getLastGrades() {
-        try {
-            await fetch("http://localhost:3000/school/grades?last=5", {
-                method: "GET",
-                credentials: "include",
-            })
-                .then((res) => res.json())
-                .then((data) => setLastGrades(data));
-        } catch (error) {
-            console.error(error);
-        }
+  async function getLastGrades() {
+    try {
+      await fetch("http://localhost:3000/school/grades?last=5", {
+        method: "GET",
+        credentials: "include",
+      })
+        .then((res) => res.json())
+        .then((data) => setLastGrades(data));
+    } catch (error) {
+      console.error(error);
     }
+  }
 
-    async function getLuckyNumber() {
-        try {
-            await fetch("http://localhost:3000/school/lucky-number", {
-                method: "GET",
-                credentials: "include",
-            })
-                .then((res) => res.json())
-                .then((data) => setLuckyNumber(data.number));
-        } catch (error) {
-            console.error(error);
-        }
+  async function getLuckyNumber() {
+    try {
+      await fetch("http://localhost:3000/school/lucky-number", {
+        method: "GET",
+        credentials: "include",
+      })
+        .then((res) => res.json())
+        .then((data) => setLuckyNumber(data.number));
+    } catch (error) {
+      console.error(error);
     }
+  }
 
-    const lastGradesForIn = () => {
-        let finalGrades: Array<Object> = [];
-        for (const subject in lastGrades) {
-            lastGrades[subject].forEach((grade: any) => {
-                finalGrades.push({
-                    subject: subject,
-                    id: grade.id,
-                    grade: grade.grade === null ? "np" : grade.grade,
-                });
-            });
-        }
-        setLastGrades(finalGrades);
-    };
+  const lastGradesForIn = useCallback(() => {
+    console.log("chuj1", lastGrades);
+    let finalGrades: any = [];
+    for (const subject in lastGrades) {
+      console.log("chuj12");
+      lastGrades[subject].forEach((grade: any) => {
+        finalGrades.push({
+          subject: subject,
+          id: grade.id,
+          grade: grade.grade === null ? "np" : grade.grade,
+        });
+      });
+    }
+    setTemp(finalGrades);
+  }, []);
 
-    useEffect(() => {
-        getLuckyNumber();
-        getAllGrades();
-        getLastGrades();
-        lastGradesForIn();
-    }, []);
+  useEffect(() => {
+    getLuckyNumber();
+    getAllGrades();
+    getLastGrades();
+  }, []);
 
-    return (
-        <>
-            <div className={classes.navbar} ref={navbarRef}>
-                <div>
-                    <LinkSection
-                        elements={[
-                            {
-                                destination: "/school/grades",
-                                label: "Oceny",
-                                icon: <Icon.Icon1CircleFill/>,
-                            },
-                            {
-                                destination: "/school/attendance",
-                                label: "Frekwencja",
-                                icon: <Icon.Fingerprint/>,
-                            },
-                            {
-                                destination: "/school/exams",
-                                label: "Sprawdziany",
-                                icon: <Icon.PenFill/>,
-                            },
-                            {
-                                destination: "/school/lessons",
-                                label: "Plan lekcji",
-                                icon: <Icon.Calendar3RangeFill/>,
-                            },
-                            {
-                                destination: "/school/messages",
-                                label: "Wiadomości",
-                                icon: <Icon.ChatLeftFill/>,
-                            },
-                        ]}
-                    />
-                </div>
-                <div className={classes.stats}>
-                    {luckyNumber !== 0 ? (
-                        <div className={classes.stat}>
-                            Szczęśliwy numerek: {luckyNumber}
-                        </div>
-                    ) : (
-                        <></>
-                    )}
-                    <div className={classes.stat}>
-                        {/*Ostatnie oceny:*/}
-                        {/*{lastGrades.map((grade) => {*/}
-                        {/*    return (*/}
-                        {/*    <p key={grade.id}>*/}
-                        {/*        {grade.subject} + ": " + {grade.grade} */}
-                        {/*    </p>*/}
-                        {/*    )*/}
-                        {/*})}*/}
-                        <div className={classes.stat}></div>
-                    </div>
-                    <p className={classes.stat}>
-                        {/*    Średnia ocen:{" " + grades.reduce((a, b) => {*/}
-                        {/*    if (!isNaN(+b.content)) {*/}
-                        {/*        return a + +b.content;*/}
-                        {/*    } else {*/}
-                        {/*        return a;*/}
-                        {/*    }*/}
-                        {/*}, 0) / grades.length}*/}
-                    </p>
-                    <p className={classes.stat}>
-                        Dni do wakacji:
-                        {" " +
-                            Math.ceil(
-                                (new Date("06/24/2023").getTime() - new Date().getTime()) /
-                                (1000 * 3600 * 24)
-                            )}
-                    </p>
-                </div>
+  useEffect(() => {
+    lastGradesForIn();
+  }, [lastGrades])
+
+  return (
+    <>
+      <div className={classes.navbar} ref={navbarRef}>
+        <div>
+          <LinkSection
+            elements={[
+              {
+                destination: "/school/grades",
+                label: "Oceny",
+                icon: <Icon.Icon1CircleFill />,
+              },
+              {
+                destination: "/school/attendance",
+                label: "Frekwencja",
+                icon: <Icon.Fingerprint />,
+              },
+              {
+                destination: "/school/exams",
+                label: "Sprawdziany",
+                icon: <Icon.PenFill />,
+              },
+              {
+                destination: "/school/lessons",
+                label: "Plan lekcji",
+                icon: <Icon.Calendar3RangeFill />,
+              },
+              {
+                destination: "/school/messages",
+                label: "Wiadomości",
+                icon: <Icon.ChatLeftFill />,
+              },
+            ]}
+          />
+        </div>
+        <div className={classes.stats}>
+          {luckyNumber !== 0 ? (
+            <div className={classes.stat}>
+              Szczęśliwy numerek: {luckyNumber}
             </div>
-        </>
-    );
+          ) : (
+            <></>
+          )}
+          <div className={classes.stat}>
+            <p>Ostatnie oceny:</p>
+            {temp.map((grade) => {
+              return <p key={grade.id}>{grade.subject + " " + grade.grade}</p>;
+            })}
+          </div>
+          <p className={classes.stat}>
+            {/*    Średnia ocen:{" " + grades.reduce((a, b) => {*/}
+            {/*    if (!isNaN(+b.content)) {*/}
+            {/*        return a + +b.content;*/}
+            {/*    } else {*/}
+            {/*        return a;*/}
+            {/*    }*/}
+            {/*}, 0) / grades.length}*/}
+          </p>
+          <p className={classes.stat}>
+            Dni do wakacji:
+            {" " +
+              Math.ceil(
+                (new Date("06/24/2023").getTime() - new Date().getTime()) /
+                  (1000 * 3600 * 24)
+              )}
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Sidebar;
