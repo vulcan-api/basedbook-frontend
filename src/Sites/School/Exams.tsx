279728e315a0b57b2a5f4a329edb48b17955abc8
Maksymilian Gala <mgwspinanie2006@wp.pl>
2023-02-28 08:58:37 +0100

    Some changes in exams and grades

--- src/Sites/School/Exams.tsx
+++ src/Sites/School/Exams.tsx
@@ -1,53 +1,53 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import LoadingSpinner from "../../Components/LoadingSpinner";
 import classes from "./Exams.module.css";
 import Wrapper from "../../Layout/Wrapper";
 
 const Exams = () => {
-  const [exams, setExams] = useState<any[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
+    const [exams, setExams] = useState<any[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    getAllExams();
-  }, []);
+    useEffect(() => {
+        getAllExams();
+    }, []);
 
-  async function getAllExams() {
-    setIsLoading(true);
-    try {
-      await fetch("http://localhost:3000/school/exams", {
-        method: "GET",
-        credentials: "include",
-      })
-        .then((res) => res.json())
-        .then((data) => setExams(data));
-    } catch (error) {
-      console.error(error);
+    async function getAllExams() {
+        setIsLoading(true);
+        try {
+            await fetch("http://localhost:3000/school/exams", {
+                method: "GET",
+                credentials: "include",
+            })
+                .then((res) => res.json())
+                .then((data) => setExams(data));
+        } catch (error) {
+            console.error(error);
+        }
+        setIsLoading(false);
     }
-    setIsLoading(false);
-  }
 
-  return (
-    <>
-      {!isLoading && (
-        <div className={classes.exams}>
-          {exams.map((exam) => {
-            return (
-              <div key={exam.id}>
-                <Wrapper className={classes.exam}>
-                  <h2>{exam.subject}</h2>
-                  <h3>{exam.type}</h3>
-                  <p>{exam.deadline}</p>
-                  <p>{exam.description}</p>
-                  <p className={classes.teacher}>{exam.teacherName}</p>
-                </Wrapper>
-              </div>
-            );
-          })}
-        </div>
-      )}
-      {isLoading && <LoadingSpinner />}
-    </>
-  );
+    return (
+        <>
+            {!isLoading && (
+                <div className={classes.exams}>
+                    {exams.map((exam) => {
+                        return (
+                            <div key={exam.id}>
+                                <Wrapper className={exam.type === "Sprawdzian" ? classes.exam : classes.quiz}>
+                                    <h2>{exam.subject}</h2>
+                                    <h3>{exam.type}</h3>
+                                    <p>{exam.deadline}</p>
+                                    <p>{exam.description}</p>
+                                    <p className={classes.teacher}>{exam.teacherName}</p>
+                                </Wrapper>
+                            </div>
+                        );
+                    })}
+                </div>
+            )}
+            {isLoading && <LoadingSpinner/>}
+        </>
+    );
 };
 
 export default Exams;
