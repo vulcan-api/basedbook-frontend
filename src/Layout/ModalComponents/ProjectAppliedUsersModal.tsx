2cb29ef7ec0bab0e684f770e22d3a24fc9478cc6
Maksymilian Gala <mgwspinanie2006@wp.pl>
2023-03-04 22:27:47 +0100

    Add display project participants

--- src/Layout/ModalComponents/ProjectAppliedUsersModal.tsx
+++ src/Layout/ModalComponents/ProjectAppliedUsersModal.tsx
@@ -4,34 +4,54 @@ import {NotificationManager} from "react-notifications";
 
 const ProjectAppliedUsersModal = (props: any) => {
 
-    const [participants, setParticipants] = useState([]);
-
-    async function getUsers() {
-        const throwObject = {};
-        const response = await fetch(
-            `http://localhost:3000/project/${props.projectId}/participants`,
-            {
-                method: "GET",
-                credentials: "include",
+        const [participants, setParticipants] = useState([{
+            "user": {
+                "id": 4,
+                "name": "Jan",
+                "surname": "Kowalski"
             }
+        }]);
+
+        async function getUsers() {
+            const throwObject = {};
+            const response = await fetch(
+                `http://localhost:3000/project/${props.projectId}/participants`,
+                {
+                    method: "GET",
+                    credentials: "include",
+                }
+            )
+                .then((res) => res.json()).then(setParticipants)
+                .catch((err) => {
+                    console.error(err);
+                    NotificationManager.error(
+                        "Wystąpił błąd podczas pobierania użytkowników!",
+                        "Błąd!",
+                        3000
+                    );
+                    return throwObject;
+                });
+        }
+
+        useEffect(() => {
+            getUsers();
+        })
+
+        return (
+            <div>
+                <ul>
+                    {participants.length === 0 && (<li>Brak użytkowników</li>)}
+                    {participants.map((participant: any, index) => {
+                        return (
+                            <li key={index}>
+                                {participant.user.name} {participant.user.surname}
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
         )
-            .then((res) => res.json()).then(setParticipants)
-            .catch((err) => {
-                console.error(err);
-                NotificationManager.error(
-                    "Wystąpił błąd podczas pobierania użytkowników!",
-                    "Błąd!",
-                    3000
-                );
-                return throwObject;
-            });
     }
-
-    useEffect(() => {
-        getUsers();
-    })
-
-    return <p>Chuj</p>;
-};
+;
 
 export default ProjectAppliedUsersModal;
